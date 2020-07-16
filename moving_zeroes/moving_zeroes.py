52d28a9d58e5229f1cc31b7fab9616506dc9ad3f
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-07-16 19:40:22 -0400

    completed single number

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,15 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    pos = 0
 
-    pass
+    for i in range(len(arr)):
+        el = arr[i]
+        
+        if el is not 0:
+            arr[pos], arr[i] = arr[i], arr[pos]
+            pos += 1
+
 
 
 if __name__ == '__main__':
